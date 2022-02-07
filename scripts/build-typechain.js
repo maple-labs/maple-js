45d59e69b9f8d157d4a79d581275010b609802f9
Jack Casey <jakcasey17@live.com>
2022-02-07 17:25:11 +0000

    Refactor scripts to js

--- scripts/build-typechain.js
+++ scripts/build-typechain.js
@@ -1,44 +1,33 @@
 // Loops through all version of all modules defined in `contract-packages` in ./config.json to determine which contracts in node_modules to run typechain against, with specific one-to-one outputs in the ./src/typechain directory.
 // Note: if this cannot be done in js, then perhaps as a `./bin/build-typechain.sh` instead.
 const path = require('path');
-const fs = require('fs');
 const shell = require('shelljs');
 const typechain = require('typechain');
-
-const getConfig = require('./getConfig');
-
-interface VersionedContract {
-  repo: string;
-  alias: string[];
-  versions: string[];
-}
-
-enum ContractAlias {
-  debtLockerV1 = 'debtLockerV1',
-  debtLockerV2 = 'debtLockerV2',
-  LoanV1 = 'LoanV1',
-  LoanV2 = 'LoanV2',
-}
-interface ContractsConfig {
-  contractPackages: Record<ContractAlias, VersionedContract>;
-}
+const path = require('path');
+const fs = require('fs');
 
 const artifactsOutputDir = path.join(__dirname, '../src/typechain');
 
-const installPackages = async (config: ContractsConfig) => {
+const getParsedConfig = () => {
+  const configPath = path.join(__dirname, '../config.json');
+  const file = fs.readFileSync(configPath);
+  return JSON.parse(file);
+};
+
+const installPackages = async (config) => {
   for (const key in config.contractPackages) {
-    const { repo, versions, alias }: VersionedContract = config.contractPackages[key as ContractAlias];
+    const { repo, versions, alias } = config.contractPackages[key];
     for (let i = 0; i < versions.length; i++) {
       await shell.exec(`yarn add --dev @maplelabs/${alias[i]}@npm:${repo}@${versions[i]}`);
     }
   }
 };
 
-const generateTypechainBindings = async (config: ContractsConfig) => {
+const generateTypechainBindings = async (config) => {
   const cwd = process.cwd();
   const aliases = [];
   for (const key in config.contractPackages) {
-    aliases.push(...config.contractPackages[key as ContractAlias].alias);
+    aliases.push(...config.contractPackages[key].alias);
   }
 
   // If doing without for loop then need => @(aliases.join('|'))/artifacts
@@ -63,7 +52,7 @@ const generateTypechainBindings = async (config: ContractsConfig) => {
 };
 
 async function buildTypechain() {
-  const config: ContractsConfig = getConfig();
+  const config = getParsedConfig();
   await installPackages(config);
   await generateTypechainBindings(config);
 }
