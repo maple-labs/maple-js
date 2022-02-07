517666dcbb4c09bbe70b606267dda33073fa0c88
Jack Casey <jakcasey17@live.com>
2022-02-07 17:25:31 +0000

    Export contracts at entry point

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import FooService from './services/foo'
+export * as debtLockerV1 from './typechain/debtLockerV1/factories/DebtLocker__factory';
+export * as debtLockerV2 from './typechain/debtLockerV2/factories/DebtLocker__factory';
 
-export { FooService }
+export * as loanV1 from './typechain/LoanV1/factories/Loan__factory';
+export * as loanV2 from './typechain/LoanV2/factories/MapleLoan__factory';
