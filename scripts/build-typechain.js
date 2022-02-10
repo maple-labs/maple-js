13a5dcbbc5e0e2730ab7131af2225ca251f0c56e
Jack Casey <jakcasey17@live.com>
2022-02-10 15:58:17 +0000

    Remove redundant code

--- scripts/build-typechain.js
+++ scripts/build-typechain.js
@@ -1,7 +1,6 @@
 // Loops through all version of all modules defined in `contract-packages` in ./config.json to determine which contracts in node_modules to run typechain against, with specific one-to-one outputs in the ./src/typechain directory.
 // Note: if this cannot be done in js, then perhaps as a `./bin/build-typechain.sh` instead.
 const path = require('path')
-const shell = require('shelljs')
 const typechain = require('typechain')
 const { readFileSync } = require('fs')
 
@@ -13,25 +12,12 @@ const getParsedConfig = () => {
   return JSON.parse(file)
 }
 
-// const installPackages = async (config) => {
-//   for (const key in config.contractPackages) {
-//     const { repo, versions, alias } = config.contractPackages[key]
-//     for (let i = 0; i < versions.length; i++) {
-//       console.log('👀 Currently installing:', alias[i]);
-//       await shell.exec(`yarn add --dev @maplelabs/${alias[i]}@npm:${repo}@${versions[i]}`)
-//     }
-//   }
-// }
-
 const generateTypechainBindings = async (config) => {
   const cwd = process.cwd()
   const aliases = []
   for (const key in config.contractPackages) {
     aliases.push(...config.contractPackages[key].alias)
   }
-
-  // If doing without for loop then need => @(aliases.join('|'))/artifacts
-  // But different versioned contracts with same name get overwritten
 
   for (let i = 0; i < aliases.length; i++) {
     const allFiles = typechain.glob(
@@ -54,7 +40,6 @@ const generateTypechainBindings = async (config) => {
 async function buildTypechain() {
   console.log('⏳ Building Typechain...')
   const config = getParsedConfig()
-  // await installPackages(config)
   await generateTypechainBindings(config)
 }
 
