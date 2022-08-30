e56e6e4a0679c8aa4473bf95f91104dcb706e05b
Jack Casey <jakcasey17@live.com>
2022-08-30 15:31:50 +0100

    Configure exports for new SCs

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,22 @@ import * as debtLockerV1Imports from './typechain/debtLockerV1'
 import * as debtLockerV2Imports from './typechain/debtLockerV2'
 import * as debtLockerV3Imports from './typechain/debtLockerV3'
 import * as fundingLockerImports from './typechain/fundingLocker'
+import * as globalsV2Imports from './typechain/globalsV2'
 import * as lateFeeCalcImports from './typechain/lateFeeCalculator'
 import * as liquidityLockerImports from './typechain/liquidityLocker'
 import * as loanV1Imports from './typechain/loanV1'
 import * as loanV2Imports from './typechain/loanV2'
 import * as loanV3Imports from './typechain/loanV3'
+import * as loanV4Imports from './typechain/loanV4'
 import * as mapleTokenImports from './typechain/mapleToken'
 import * as mapleGlobalsImports from './typechain/mapleGlobals'
 import * as mapleRewardsImports from './typechain/mplRewards'
 import * as poolImports from './typechain/pool'
+import * as poolV2Imports from './typechain/poolV2'
 import * as premiumCalcImports from './typechain/premiumCalculator'
 import * as repaymentCalcImports from './typechain/repaymentCalculator'
 import * as stakeLockerImports from './typechain/stakeLocker'
+import * as withdrawalManagerImports from './typechain/withdrawal-manager'
 import * as xmplImports from './typechain/xmpl'
 import * as environmentMocksImports from './typechain/environmentMocks'
 
@@ -55,6 +59,10 @@ const fundingLocker = {
   factory: fundingLockerImports.FundingLockerFactory__factory
 }
 
+const globalsV2 = {
+  core: globalsV2Imports.MapleGlobals__factory
+}
+
 const lateFeeCalc = {
   core: lateFeeCalcImports.LateFeeCalc__factory
 }
@@ -84,6 +92,15 @@ const loanV3 = {
   refinancer: loanV3Imports.Refinancer__factory
 }
 
+const loanV4 = {
+  core: loanV4Imports.MapleLoan__factory,
+  factory: loanV4Imports.MapleLoanFactory__factory,
+  initializer: loanV4Imports.MapleLoanInitializer__factory,
+  migrator: loanV4Imports.MapleLoanV4Migrator__factory,
+  feeManager: loanV4Imports.MapleLoanFeeManager__factory,
+  refinancer: loanV4Imports.Refinancer__factory
+}
+
 const mapleGlobals = {
   core: mapleGlobalsImports.MapleGlobals__factory
 }
@@ -103,6 +120,27 @@ const pool = {
   lib: poolImports.PoolLib__factory
 }
 
+const poolV2 = {
+  core: poolV2Imports.Pool__factory,
+  delegateCover: poolV2Imports.PoolDelegateCover__factory,
+  deployer: poolV2Imports.PoolDeployer__factory,
+}
+
+const poolV2Manager = {
+  core: poolV2Imports.PoolManager__factory,
+  factory: poolV2Imports.PoolManagerFactory__factory,
+  initializer: poolV2Imports.PoolManagerInitializer__factory,
+  storage: poolV2Imports.PoolManagerStorage__factory,
+}
+
+const loanManager = {
+  core: poolV2Imports.LoanManager__factory,
+  factory: poolV2Imports.LoanManagerFactory__factory,
+  initializer: poolV2Imports.LoanManagerInitializer__factory,
+  storage: poolV2Imports.LoanManagerStorage__factory,
+  transition: poolV2Imports.TransitionLoanManager__factory
+}
+
 const premiumCalc = {
   core: premiumCalcImports.PremiumCalc__factory
 }
@@ -114,6 +152,13 @@ const repaymentCalculator = {
 const stakeLocker = {
   core: stakeLockerImports.StakeLocker__factory,
   factory: stakeLockerImports.StakeLockerFactory__factory
+}
+
+const withdawalManager = {
+  core: withdrawalManagerImports.WithdrawalManager__factory,
+  factory: withdrawalManagerImports.WithdrawalManagerFactory__factory,
+  initializer: withdrawalManagerImports.WithdrawalManagerInitializer__factory,
+  storage: withdrawalManagerImports.WithdrawalManagerStorage__factory
 }
 
 const bPool = {
@@ -163,6 +208,7 @@ interface ContractTypes {
   debtLockerV2: debtLockerV2Imports.DebtLocker
   debtLockerV2Factory: debtLockerV2Imports.DebtLockerFactory
   debtLockerV2Initializer: debtLockerV2Imports.DebtLockerInitializer
+  globalsV2: globalsV2Imports.MapleGlobals__factory
   liquidityLocker: liquidityLockerImports.LiquidityLocker
   liquidityLockerFactory: liquidityLockerImports.LiquidityLockerFactory
   loanV2: loanV2Imports.MapleLoan
@@ -171,6 +217,12 @@ interface ContractTypes {
   loanV3Factory: loanV3Imports.MapleLoanFactory
   loanV3Initializer: loanV3Imports.MapleLoanInitializer
   loanV3Refinancer: loanV3Imports.Refinancer
+  loanV4: loanV4Imports.MapleLoan__factory
+  loanV4Factory: loanV4Imports.MapleLoanFactory__factory
+  loanV4Initializer: loanV4Imports.MapleLoanInitializer__factory
+  loanV4Migrator: loanV4Imports.MapleLoanV4Migrator__factory
+  loanV4FeeManager: loanV4Imports.MapleLoanFeeManager__factory
+  loanV4Refinancer: loanV4Imports.Refinancer__factory
   mapleToken: mapleTokenImports.MapleToken
   mapleGlobals: mapleGlobalsImports.MapleGlobals
   mapleRewards: mapleRewardsImports.MplRewards
@@ -180,6 +232,27 @@ interface ContractTypes {
   xmpl: xmplImports.XMPL
   pool: poolImports.Pool
   poolFactory: poolImports.PoolFactory
+  // PoolV2:poolV2
+  poolV2Factory: poolV2Imports.Pool__factory
+  delegateCover: poolV2Imports.PoolDelegateCover__factory
+  poolV2deployer: poolV2Imports.PoolDeployer__factory
+  // PoolV2:poolV2Manager
+  poolV2Manager: poolV2Imports.PoolManager__factory
+  poolV2ManagerFactory: poolV2Imports.PoolManagerFactory__factory
+  poolV2ManagerInitializer: poolV2Imports.PoolManagerInitializer__factory
+  poolV2ManagerStorage: poolV2Imports.PoolManagerStorage__factory
+  // PoolV2:loanManager
+  loanManager: poolV2Imports.LoanManager__factory
+  loanManagerfactory: poolV2Imports.LoanManagerFactory__factory
+  loanManagerInitializer: poolV2Imports.LoanManagerInitializer__factory
+  loanManagerStorage: poolV2Imports.LoanManagerStorage__factory
+  loanManagerTransition: poolV2Imports.TransitionLoanManager__factory
+
+  withdrawalManager: withdrawalManagerImports.WithdrawalManager__factory
+  withdrawalManagerFactory: withdrawalManagerImports.WithdrawalManagerFactory__factory
+  withdrawalManagerInitializer: withdrawalManagerImports.WithdrawalManagerInitializer__factory
+  withdrawalManagerStorage: withdrawalManagerImports.WithdrawalManagerStorage__factory
+
   erc20: environmentMocksImports.MintSpecialToken
 }
 
@@ -193,18 +266,24 @@ export {
   debtLockerV3,
   erc20,
   fundingLocker,
+  globalsV2,
   lateFeeCalc,
   liquidityLocker,
   loanV1,
   loanV2,
   loanV3,
+  loanV4,
+  loanManager,
   mapleGlobals,
   mapleRewards,
   mapleToken,
   pool,
+  poolV2,
+  poolV2Manager,
   premiumCalc,
   repaymentCalculator,
   stakeLocker,
+  withdawalManager,
   uniswapRouterV2,
   xmpl,
   ContractTypes,
