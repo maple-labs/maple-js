bbbcf98ba18e6ce32f1b347e5d038de37c413aa5
Callum Hyland <hello@callumhyland.com>
2022-11-23 10:50:31 +0000

    Add migrationHelpers to typechain

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ import * as stakeLockerImports from './typechain/stakeLocker'
 import * as poolV2Imports from './typechain/poolV2'
 import * as withdrawalManagerImports from './typechain/withdrawalManager'
 import * as environmentMocksImports from './typechain/environmentMocks'
+import * as migrationHelpersImports from './typechain/migrationHelpers'
 
 // Addresses
 import goerliAddresses from './addresses/goerli'
@@ -118,6 +119,10 @@ const mapleRewards = {
 
 const mapleToken = {
   factory: mapleTokenImports.MapleToken__factory
+}
+
+const migrationHelpers = {
+  factory: migrationHelpersImports.MigrationHelper__factory
 }
 
 const xmpl = {
@@ -256,6 +261,8 @@ interface ContractTypes {
   // environmentMocks
   bPool: environmentMocksImports.BPool
   erc20: environmentMocksImports.MintSpecialToken
+  // PoolV2: migrationHelper
+  migrationHelpers: migrationHelpersImports.MigrationHelper
 }
 
 export {
@@ -276,6 +283,7 @@ export {
   mapleGlobalsV2,
   mapleRewards,
   mapleToken,
+  migrationHelpers,
   xmpl,
   pool,
   premiumCalc,
