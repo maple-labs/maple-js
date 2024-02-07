2c989b5b5e5b679732976e481229c9363219ad77
Jack Casey <jak@maple.finance>
2024-02-07 18:08:52 +0100

    example usage of generateTransactionData

--- src/helpers/serialiseExampleUse.ts
+++ src/helpers/serialiseExampleUse.ts
@@ -1,16 +1,8 @@
-import { JsonRpcProvider } from 'ethers'
+import { JsonRpcProvider, Transaction } from 'ethers'
 
 import { UnsignedTransactionBundle, generateTransactionData } from './serialiseTransaction'
 
-// type SignatureLike = {
-//   r: string
-//   s?: string
-//   _vs?: string
-//   recoveryParam?: number
-//   v?: number
-// }
-
-// const HSMSigningAPICall = (serializedTx: string): SignatureLike => ({ r: '0x', s: '0x', v: 0 })
+import { Wallet } from 'ethers'
 
 const chainIds = {
   mainnet: 1,
@@ -21,35 +13,53 @@ const chainIds = {
   localhost: 31337
 }
 
-async function main() {
-  // Setup
-  // 📋 Need to parametise all this
-  const provider = new JsonRpcProvider(rpcURL)
-  const poolAddress = '0x5d522516d9119a6e44d8d47224274e7b5b648649'
-  const depositAmount = BigInt('1')
-  const walletAddress = '0xaa5aa072369a3f34fca3926ddf31977fad95022d'
+const mnemonic = ''
 
-  // Serialize the transaction (unsigned)
+async function main() {
+  // 🚨 1) Setup 🚨
+  // 📋 Need to parametise all this
+  const provider = new JsonRpcProvider('https://eth-goerli.g.alchemy.com/v2/FImNHlvh-EzuzzK02dMWi6TBt9xh6mGM')
+  const wallet = Wallet.fromPhrase(mnemonic)
+
+  const walletWithProvider = wallet.connect(provider)
+  const poolAddress = '0x4ff5637548e48abaf07baf4a5769b8abbe03a322'
+  // const poolAddress = '0x5d522516d9119a6e44d8d47224274e7b5b648649'
+  const amount = BigInt('1000000')
+  const walletAddress = '0xaA5aA072369A3F34fcA3926DDf31977fAD95022D'
+
+  // 🚨 2) Serialize the transaction (unsigned) 🚨
   const { txBytes, txInstance }: UnsignedTransactionBundle = await generateTransactionData({
     provider,
     walletAddress,
     chainId: chainIds.goerli,
     contractAddress: poolAddress,
     type: 'poolDeposit',
-    depositAmount
+    params: {
+      depositAmount: amount
+    }
   })
 
-  console.log('😡', { txBytes, txInstance })
+  // const { txBytes, txInstance }: UnsignedTransactionBundle = await generateTransactionData({
+  //   provider,
+  //   walletAddress,
+  //   chainId: chainIds.goerli,
+  //   contractAddress: poolAddress,
+  //   type: 'poolWithdrawal',
+  //   params: {
+  //     withdrawalAmount: amount
+  //   }
+  // })
 
-  // // Use the serialised string to get a signature from the HSM API
-  // const { r, s, v }: SignatureLike = HSMSigningAPICall(serialisedString)
+  console.log('🚨 :::', { txBytes, txInstance })
 
-  // // 🚨 Combine the signature with the unsigned TX object to create the serialised signed TX
-  // const signedTx = ethers.utils.serializeTransaction(unsignedTx, { r, s, v })
+  // Sign the transaction
+  const deserializeTx = Transaction.from(txBytes)
+  const signedTx = await walletWithProvider.signTransaction(deserializeTx)
+  console.log('✍🏼 :::', { signedTx })
 
-  // // Execute the signed transaction
-  // const txReceipt = provider.sendTransaction(signedTx)
-  // console.log('Transaction receipt:', txReceipt)
+  // 🚨 4) Broadcast the transaction 🚨
+  const txResponse = await provider.broadcastTransaction(signedTx)
+  console.log('#️⃣ :::', { trasnactionHash: txResponse.hash })
 }
 
 main()
