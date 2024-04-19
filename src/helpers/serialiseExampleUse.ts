e73f8c91d7fbba55ae90c38cf79da4d03161e197
Jack Casey <jak@maple.finance>
2024-04-19 13:09:24 +0400

    clean up example

--- src/helpers/serialiseExampleUse.ts
+++ src/helpers/serialiseExampleUse.ts
@@ -12,20 +12,17 @@ import {
   broadcastSignedTransaction
 } from './serialiseTransaction'
 
+import addresses from '../addresses/sepolia-prod'
+
 dotenv.config()
 
 // Amounts
-const APPROVE = BigNumber.from(200e6)
 const AMOUNT = BigNumber.from(1e6) // 1 USDC / micro eth amount
-const ZERO = BigNumber.from(0)
-
-// const poolAddress = '0xb648f9b22135523483da7a4e0600d2613b3945f2'
-// const walletAddress = '0xf1b32ebe86d83f9db1fa8f806b60bc20a26d2b89'
 
 // Addresses
-// const poolAddress = '0x722da756e3f615dc1fc8d84061e25bf0f181bdfb' // example pool in sepolia-prod
-const poolAddress = '0x8c527587bfea22da8968e65a1b768f7bd9fc8740'
-const walletAddress = '0xaA5aA072369A3F34fcA3926DDf31977fAD95022D'
+const poolAddress = '0x722da756e3f615dc1fc8d84061e25bf0f181bdfb' // example pool in sepolia-prod
+const walletAddress = '0xfoo'
+const usdcAddress = addresses.USDC
 
 async function main() {
   // 🚨 1) Setup 🚨
@@ -40,27 +37,26 @@ async function main() {
   const walletWithProvider = wallet.connect(provider)
 
   // 🚨 2) Serialize the transaction (unsigned) 🚨
+  const { txBytes }: UnsignedTransactionBundle = await generateUnsignedTransactionData({
+    provider,
+    walletAddress,
+    contractAddress: usdcAddress, // Asset to approve spending by pool (eg: usdc or weth)
+    type: 'poolApprove',
+    params: {
+      amount: AMOUNT,
+      spender: poolAddress
+    }
+  })
 
   // const { txBytes }: UnsignedTransactionBundle = await generateUnsignedTransactionData({
   //   provider,
   //   walletAddress,
-  //   contractAddress: poolAddress,
-  //   type: 'poolApprove',
+  //   contractAddress: poolAddress, // address of the pool contract for depositing funds
+  //   type: 'poolDeposit',
   //   params: {
-  //     amount: APPROVE,
-  //     spender: poolAddress
+  //     depositAmount: AMOUNT
   //   }
   // })
-
-  const { txBytes }: UnsignedTransactionBundle = await generateUnsignedTransactionData({
-    provider,
-    walletAddress,
-    contractAddress: poolAddress,
-    type: 'poolDeposit',
-    params: {
-      depositAmount: AMOUNT
-    }
-  })
 
   // const { txBytes, txInstance }: UnsignedTransactionBundle = await generateTransactionData({
   //   provider,
